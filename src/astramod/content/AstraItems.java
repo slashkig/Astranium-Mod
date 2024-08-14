819b919bbc417734a311b07c8528a62d01e40a78
IAmRustee <wewen305@gmail.com>
2024-08-14 11:58:48 +0800

    Update AstraItems.java
    added iron and hematite

--- src/astramod/content/AstraItems.java
+++ src/astramod/content/AstraItems.java
@@ -5,7 +5,7 @@ import arc.graphics.Color;
 import arc.util.Log;
 
 public class AstraItems {
-	public static Item testium;
+	public static Item testium, hematite;
 
 	public static void load() {
 		Log.info("Loading items");
@@ -13,6 +13,15 @@ public class AstraItems {
 		testium = new Item("testium", Color.valueOf("ff00ff")) {{
 			hardness = 1;
 			description = "Test item for Mindustry";
+		}};
+		hematite = new Item("hematite", Color.valueOf("ffffff")) {{
+			hardness = 1;
+			description = "Unrefined iron ore. Used in basic construction.";
+			cost = 0.7f;
+		}};
+		iron = new Item("iron", Color.valueOf("202020")) {{
+			description = "Smelted iron ore. Used in simple industrial buildings.";
+			cost = 1;
 		}};
 	}
 }
