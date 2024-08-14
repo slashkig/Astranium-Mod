98e8dcc019b96d1c4220b315f98a2797f59f5cbc
IAmRustee <wewen305@gmail.com>
2024-08-14 12:36:57 +0800

    Update AstraBlocks.java
    added iron forge

--- src/astramod/content/AstraBlocks.java
+++ src/astramod/content/AstraBlocks.java
@@ -2,15 +2,33 @@ package astramod.content;
 
 import mindustry.world.Block;
 import mindustry.world.blocks.environment.OreBlock;
+import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.draw.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.content.Fx;
 import astramod.content.AstraItems;
 import arc.util.Log;
 
 public class AstraBlocks {
-	public static Block oreTestium;
+	public static Block oreTestium, ironForge;
 
 	public static void load() {
 		Log.info("Loading blocks");
 
 		oreTestium = new OreBlock(AstraItems.testium);
+
+		ironForge = new GenericCrafter("iron-forge") {{
+			requirements(Catergory.crafting with(AstraItems.hematite, 20));
+			craftEffect = Fx.formsmoke;
+			outputItem = new ItemStack(AstraItem.iron, 1);
+			craftTime = 60f;
+			size = 2;
+			hasPower = hasItems = true;
+			drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffc099")));
+
+			consumeItems(with(AstraItems.hematite, 2));
+			consumePower(0.60f);
+		}};
 	}
 }
