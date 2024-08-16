eeaa0ba68eaf8f3eb40ba3e0d1defde37846eebd
slashkig <161779209+slashkig@users.noreply.github.com>
2024-08-16 00:06:44 -0400

    Added iron walls and graphite maker
    Also tweaked the specs of some blocks

--- src/astramod/content/AstraBlocks.java
+++ src/astramod/content/AstraBlocks.java
@@ -19,7 +19,7 @@ import arc.graphics.*;
 import astramod.content.AstraItems;
 
 public class AstraBlocks {
-	public static Block oreTestium, oreHematite, ironForge, blastFurnace, ironDrill, hematiteWall, hematiteWallLarge, testblaster;
+	public static Block oreTestium, oreHematite, ironForge, magnetiteSynthesizer, blastFurnace, ironDrill, hematiteWall, hematiteWallLarge, ironWall, IronWallLarge, testblaster;
 
 	public static void load() {
 		Log.info("Loading blocks");
@@ -29,24 +29,36 @@ public class AstraBlocks {
 		oreHematite = new OreBlock(AstraItems.hematite);
 		
 		ironForge = new GenericCrafter("iron-forge") {{
-			requirements(Category.crafting, ItemStack.with(AstraItems.hematite, 20));
+			requirements(Category.crafting, ItemStack.with(AstraItems.hematite, 20, Items.lead, 10));
 			craftEffect = Fx.formsmoke;
 			outputItem = new ItemStack(AstraItems.iron, 1);
-			craftTime = 60f;
+			craftTime = 40f;
 			size = 2;
 			hasPower = hasItems = true;
 			drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffc099")));
 			squareSprite = false;
 
 			consumeItems(ItemStack.with(AstraItems.hematite, 2));
-			consumePower(0.60f);
+			consumePower(0.50f);
 		}};
 
+		magnetiteSynthesizer = new GenericCrafter("magnetite-synthesizer") {{
+			requirements(Category.crafting, ItemStack.with(AstraItems.iron, 50, Items.silicon, 30, Items.lead, 40));
+			craftEffect = Fx.pulverizeMedium;
+			outputItem = new ItemStack(AstraItems.magnetite, 1);
+			craftTime = 60f;
+			size = 2;
+			hasPower = hasItems = true;
+
+			consumeItems(ItemStack.with(AstraItems.hematite, 2, Items.graphite, 1));
+			consumePower(1.2f);
+		}};
+		
 		blastFurnace = new GenericCrafter("blast-furnace") {{
 			requirements(Category.crafting, ItemStack.with(AstraItems.iron, 80, Items.graphite, 40, Items.silicon, 60, Items.titanium, 30));
 			craftEffect = Fx.formsmoke;
 			outputItem = new ItemStack(AstraItems.steel, 1);
-			craftTime = 100f;
+			craftTime = 70f;
 			size = 3;
 			hasPower = hasItems = true;
 			drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffef99")));
@@ -57,7 +69,7 @@ public class AstraBlocks {
 		}};
 
 		ironDrill = new Drill("iron-drill") {{
-			requirements(Category.production, ItemStack.with(Items.copper, 25, Items.graphite, 20, AstraItems.iron, 25));
+			requirements(Category.production, ItemStack.with(AstraItems.iron, 25, Items.copper, 25, Items.graphite, 20));
 			drillTime = 320;
 			size = 3;
 			hasPower = true;
@@ -82,10 +94,23 @@ public class AstraBlocks {
 			envDisabled |= Env.scorching;
 		}};
 
+		ironWall = new Wall("iron-wall") {{
+			requirements(Category.defense, ItemStack.with(AstraItems.hematite, 6));
+			health = 120 * 4;
+			envDisabled |= Env.scorching;
+		}};
+		
+		ironWallLarge = new Wall("iron-wall-large") {{
+			requirements(Category.defense, ItemStack.mult(hematiteWall.requirements, 4));
+			health = 120 * 16;
+			size = 2;
+			envDisabled |= Env.scorching;
+		}};
+		
 		testblaster = new ItemTurret("testblaster") {{
 			requirements(Category.turret, ItemStack.with(AstraItems.testium, 1000));
 			ammo(
-				AstraItems.testium, new BasicBulletType(10f, 100000) {{
+				AstraItems.testium, new BasicBulletType(10f, 10000000) {{
 					lifetime = 100f;
 					width = 20f;
 					height = 25f;
@@ -104,13 +129,13 @@ public class AstraBlocks {
 				}});
 			}};
 
-			maxAmmo = 500;
+			maxAmmo = 1000;
 			recoil = 2.5f;
 			reload = 6f;
 			rotateSpeed = 5f;
 			range = 300f;
 			size = 2;
-			health = 1000000;
+			health = 100000000;
 			shootY = 7.5f;
 			ammoUseEffect = Fx.none;
 			shootSound = Sounds.missile;
