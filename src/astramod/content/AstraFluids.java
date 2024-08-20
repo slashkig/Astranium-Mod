182d304ec81f1c64aba3921b49e72727f3224fcc
slashkig <161779209+slashkig@users.noreply.github.com>
2024-08-20 12:09:39 -0400

    Create file with ferrofluid

---
+++ src/astramod/content/AstraFluids.java
@@ -0,0 +1,21 @@
+package astramod.content;
+
+import mindustry.type.Liquid;
+import mindustry.content.StatusEffects;
+import arc.graphics.Color;
+
+public class AstraFluids {
+	public static Liquid ferrofluid;
+	
+	public static void load() {
+		ferrofluid = new Liquid("ferrofluid", Color.valueOf("080000")) {{
+			effect = StatusEffects.none; // Need to add status effect
+			viscosity = 0.25f;
+			heatCapacity = 0.65f;
+			flammability = 0.6f;
+			explosiveness = 0.2f;
+			boilPoint = 0.7f;
+			gasColor = Color.grays(0.1f);
+		}};
+	}
+}
